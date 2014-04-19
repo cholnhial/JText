71c1d451fab95ebae3d3b17199d2665346f26af7
Chol Nhial <chol.nhial.chol@gmail>
2014-04-19 20:01:37 +1000

    JText UI created

--- src/jtext/JText.java
+++ src/jtext/JText.java
@@ -4,18 +4,49 @@
  */
 package jtext;
 
+import javax.swing.*;
 /**
  *
  * @author chol
  */
-public class JText {
+public class JText extends JFrame {
 
+   
+   public JText() {
+       super("JText");
+       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       setSize(640,480);
+       
+       JPanel panel = new JPanel();
+       JMenuBar menubar = new JMenuBar();
+       
+       /* Create menu bar */
+       JMenuItem menuItemSave = new JMenuItem("Save");
+       JMenuItem menuItemOpen = new JMenuItem("Open");
+       JMenuItem menuItemExit = new JMenuItem("Exit");
+       JMenu menuFile = new JMenu("File");
+       menubar.add(menuFile);
+       menuFile.add(menuItemSave);
+       menuFile.add(menuItemOpen);
+       menuFile.addSeparator();
+       menuFile.add(menuItemExit);
+       add("North", menubar);
+       
+       /* Create and add text area */
+       JTextArea textArea = new JTextArea(8, 40);
+       JScrollPane textAreaScrollPane = new JScrollPane(textArea, 
+               ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+               ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS
+               );
+       add("Center", textAreaScrollPane);
+       setVisible(true);
+      
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World!\n");
-        System.in.read()
+        JText jtext = new JText();
     }
 }
